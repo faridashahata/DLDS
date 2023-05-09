16e17964405d26503cd80534411c1c0d9c5f86f6
Farida <shahata@kth.se>
2023-05-09 16:33:07 +0200

    training resnet50 unsuccessful

--- classification.py
+++ classification.py
@@ -4,8 +4,9 @@ import numpy as np
 import os
 from PIL import Image
 import cv2
+#import tf.models as tfm
+#import tensorflow_models as tfm
 
-#print(os.listdir())
 
 data_list = []
 normal_list = []
@@ -83,14 +84,21 @@ def create_files(file_names, folder_path):
         image_file = cv2.imread(im_path)
         cv2.imwrite(new_im_path, image_file)
 
+    # Make sure the folder contains a file for each of the 44 categories:
+    for file in filenames:
+        new_folder_path = os.path.join(new_path, file)
+        if not os.path.exists(new_folder_path):
+            os.makedirs(new_folder_path)
+
     return
 
 
 
 
-# create_files(train, 'training_data/')
-# create_files(val, 'validation_data/')
-#
+# Run the following lines once (alternatively clear files for different runs/draws of data splits):
+#create_files(train, 'training_data/')
+#create_files(val, 'validation_data/')
+
 
 # This is to check if a file is empty or not incase we want to either repopulate it or delete its contents for new runs
 # print(os.path.getsize(new_path) == 0)
@@ -107,6 +115,7 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
     labels='inferred',
     label_mode='categorical',
     batch_size=32,
+    #batch_size=len(train),
     image_size=(224, 224))
 
 
@@ -115,6 +124,7 @@ validation_ds = tf.keras.utils.image_dataset_from_directory(
     labels='inferred',
     label_mode='categorical',
     batch_size=32,
+    #batch_size=len(val),
     image_size=(224, 224))
 
 # This tries to imitate the way tf builds classes [not 100% sure it is correct]
@@ -129,18 +139,133 @@ def get_classes(folder_path):
 print(get_classes('training_data/'))
 
 class_names = get_classes('training_data/')
-
+# Seems like this does the trick (for some reason, it was not working)
+class_names = np.array(train_ds.class_names)
+print("class names", class_names)
 
 # Show the first nine images and labels from the training set:
 ## following steps in: https://www.tensorflow.org/tutorials/images/transfer_learning
 
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(images[i].numpy().astype("uint8"))
-        #print("labels", class_names[tf.math.argmax(labels[i])])
-        plt.title(class_names[tf.math.argmax(labels[i])])
-        plt.axis("off")
-plt.show()
+# plt.figure(figsize=(10, 10))
+# for images, labels in train_ds.take(1):
+#     for i in range(9):
+#         ax = plt.subplot(3, 3, i + 1)
+#         plt.imshow(images[i].numpy().astype("uint8"))
+#         #print("labels", class_names[tf.math.argmax(labels[i])])
+#         plt.title(class_names[tf.math.argmax(labels[i])])
+#         plt.axis("off")
+# plt.show()
 
+
+# Create test data:
+val_batches = tf.data.experimental.cardinality(validation_ds)
+print('Number of val batches: %d' % val_batches)
+test_dataset = validation_ds.take(val_batches // 5)
+validation_data = validation_ds.skip(val_batches // 5)
+
+print('Number of validation batches: %d' % tf.data.experimental.cardinality(validation_data))
+print('Number of test batches: %d' % tf.data.experimental.cardinality(test_dataset))
+
+
+# NORMALIZE DATA:
+
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y)) # Where x—images, y—labels.
+val_ds = validation_ds.map(lambda x, y: (normalization_layer(x), y)) # Where x—images, y—labels.
+
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+# Download the classifer:
+
+mobilenet_v2 ="https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4"
+inception_v3 = "https://tfhub.dev/google/imagenet/inception_v3/classification/5"
+resnet50 = "https://tfhub.dev/tensorflow/resnet_50/classification/1"
+classifier_model = resnet50
+
+IMAGE_SHAPE = (224, 224)
+import tensorflow_hub as hub
+
+
+# Download the headless model:
+
+feature_extractor_model = resnet50
+
+feature_extractor_layer = hub.KerasLayer(
+feature_extractor_model,
+input_shape=(224, 224, 3),
+trainable=False)
+
+# for image_batch, labels_batch in train_ds:
+#
+#   feature_batch = feature_extractor_layer(image_batch)
+#
+
+# Attach a classification head:
+num_classes = len(class_names)
+
+model = tf.keras.Sequential([
+  feature_extractor_layer,
+  tf.keras.layers.Dense(num_classes)
+])
+
+model.summary()
+
+# for image_batch, labels_batch in train_ds:
+#     predictions = model(image_batch)
+#     print("predictions shape", predictions.shape)
+#     break
+
+# Train the model:
+
+model.compile(
+  optimizer=tf.keras.optimizers.Adam(),
+  #loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+     loss='categorical_crossentropy',
+  metrics=['acc'])
+
+NUM_EPOCHS = 10
+
+history = model.fit(train_ds,
+                   validation_data=val_ds,
+                    epochs=NUM_EPOCHS)
+
+
+
+
+
+
+
+
+# To load a single batch:
+# for image_batch, labels_batch in train_ds:
+#     x_train = image_batch.numpy()
+#     y_train = labels_batch.numpy()
+#     break
+#
+# for image_batch, labels_batch in validation_ds:
+#     x_val = image_batch.numpy()
+#     y_val = labels_batch.numpy()
+#     break
+#
+# print("x_train", len(x_train))
+# print("y_train", len(y_train))
+#
+# print("x_train", x_train[1].shape)
+#print("y_train", y_train.shape())
+
+
+
+
+# USE https://www.tensorflow.org/tutorials/images/transfer_learning_with_hub
+
+# useful tutorial, another model: https://www.kaggle.com/code/bulentsiyah/dogs-vs-cats-classification-vgg16-fine-tuning
+
+# Tutorial using pretrained convnext
+# https://www.kaggle.com/code/shrijeethsuresh/brain-tumor-convnext-44-classes-99-4-acc
+
+# Tutorial with transfer learning on tumor data: To follow general steps in:
+# https://www.kaggle.com/code/sanandachowdhury/transfer-learning-brain-tumor-classification
